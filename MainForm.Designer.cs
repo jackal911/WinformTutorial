e3b4ae4a93b6e8a29d4e75bea58bb9de7f84d6d7
balor00 <thisisdiss@gmail.com>
2022-07-06 17:59:06 +0900

    first commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace GenericView
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.ui_canvas2D = new Canvas.Canvas2D();
+            this.ui_toolbar = new System.Windows.Forms.ToolStrip();
+            this.ui_toolOpen = new System.Windows.Forms.ToolStripButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.ui_toolbar.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.ui_canvas2D);
+            this.splitContainer1.Size = new System.Drawing.Size(979, 507);
+            this.splitContainer1.SplitterDistance = 326;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(3, 66);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(320, 395);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // ui_canvas2D
+            // 
+            this.ui_canvas2D.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ui_canvas2D.CanvasBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ui_canvas2D.CommandText = null;
+            this.ui_canvas2D.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ui_canvas2D.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ui_canvas2D.DrawCanvasTextSize = 15;
+            this.ui_canvas2D.DrawLastToolMode = -1;
+            this.ui_canvas2D.DrawToolMode = -1;
+            this.ui_canvas2D.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.ui_canvas2D.ForeColor = System.Drawing.SystemColors.Window;
+            this.ui_canvas2D.IsAreaProccessing = false;
+            this.ui_canvas2D.IsCenterAxis = true;
+            this.ui_canvas2D.IsDraw3DAxis = false;
+            this.ui_canvas2D.IsMoveEntitySelection = true;
+            this.ui_canvas2D.IsOSnapEnable = true;
+            this.ui_canvas2D.IsPickProccessing = false;
+            this.ui_canvas2D.IsShortcutKeyMode = false;
+            this.ui_canvas2D.IsStartOrthogonal = false;
+            this.ui_canvas2D.LastOSnap = null;
+            this.ui_canvas2D.Location = new System.Drawing.Point(0, 0);
+            this.ui_canvas2D.Name = "ui_canvas2D";
+            this.ui_canvas2D.SelectPartCtrl = null;
+            this.ui_canvas2D.ShowInputText = false;
+            this.ui_canvas2D.ShowProperty = true;
+            this.ui_canvas2D.Size = new System.Drawing.Size(648, 507);
+            this.ui_canvas2D.SymbolName = null;
+            this.ui_canvas2D.TabIndex = 0;
+            this.ui_canvas2D.ZoomMode = -1;
+            // 
+            // ui_toolbar
+            // 
+            this.ui_toolbar.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ui_toolbar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ui_toolOpen});
+            this.ui_toolbar.Location = new System.Drawing.Point(0, 0);
+            this.ui_toolbar.Name = "ui_toolbar";
+            this.ui_toolbar.Size = new System.Drawing.Size(1005, 25);
+            this.ui_toolbar.TabIndex = 1;
+            this.ui_toolbar.Text = "toolStrip1";
+            // 
+            // ui_toolOpen
+            // 
+            this.ui_toolOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ui_toolOpen.Image = ((System.Drawing.Image)(resources.GetObject("ui_toolOpen.Image")));
+            this.ui_toolOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ui_toolOpen.Name = "ui_toolOpen";
+            this.ui_toolOpen.Size = new System.Drawing.Size(23, 22);
+            this.ui_toolOpen.Text = "toolStripButton1";
+            this.ui_toolOpen.ToolTipText = "Open";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Location = new System.Drawing.Point(14, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(979, 507);
+            this.panel1.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 552);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ui_toolbar);
+            this.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Generic View";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ui_toolbar.ResumeLayout(false);
+            this.ui_toolbar.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ToolStrip ui_toolbar;
+        private System.Windows.Forms.ToolStripButton ui_toolOpen;
+        private System.Windows.Forms.Panel panel1;
+        private Canvas.Canvas2D ui_canvas2D;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CanvasModel;
+using System.IO;
+using Canvas;
+
+namespace GenericView
+{
+    public partial class MainForm : Form
+    {
+        #region 맴버
+        #endregion
+
+        #region 속성
+        #endregion
+        
+        #region 생성자
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\sgkim\Desktop\이준호\GenericView\Sample\AD1P-BL6A-1-1P.gen";
+            string[] f = File.ReadAllLines(path);
+            
+            TreeNode rootNode = new TreeNode("root");
+            int index = 0; // ref에 얹기 위해 초기화. 여기선 쓸모없고 내부에서 사용 목적
+            int id = 0; // // ref에 얹기 위해 초기화. 여기선 쓸모없고 내부에서 사용 목적
+            addNode(f, ref rootNode, ref index, ref id);
+            treeView1.Nodes.Add(rootNode);
+            //printAllNodes(rootNode);
+            //findNodeByText(rootNode, "CONTOUR");
+            parseAndDraw(rootNode);
+        }
+
+        class nodeTag
+        {
+            public int id { get; set; }
+        }
+
+        // 제네릭 파일 조회해서 트리만드는 함수
+        void addNode(string[] lines, ref TreeNode parentNode, ref int index, ref int id)
+        {
+            while (index < lines.Length)
+            {
+                string sbj = lines[index];
+                index++;
+                if(!sbj.Contains('='))
+                {                    
+                    if (sbj.StartsWith("END_OF_"))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        sbj = sbj.Replace("START_OF_", "");
+                        sbj = sbj.Replace("_DATA", "");
+                        TreeNode childNode = new TreeNode(sbj);
+                        if(sbj=="CONTOUR"||sbj=="STRING")
+                        {
+                            nodeTag tag = new nodeTag();
+                            tag.id = id;
+                            childNode.Tag = tag;
+                        }
+                        addNode(lines, ref childNode, ref index, ref id);
+                        parentNode.Nodes.Add(childNode);
+                    }
+                }
+                else
+                {
+                    TreeNode childNode = new TreeNode(sbj);
+                    if (parentNode.Text == "CONTOUR" || parentNode.Text == "STRING")
+                    {
+                        nodeTag tag = new nodeTag();
+                        tag.id = id;
+                        childNode.Tag = tag;
+                        if (sbj.StartsWith("V=") || sbj.StartsWith("STRING="))
+                        {
+                            id++;
+                        }
+                    }
+                    parentNode.Nodes.Add(childNode);
+                }
+            }
+        }
+
+        // 데이터 제목 찾아서 하위 노드들 콘솔에 찍는 함수 - Built-in에는 Key로 조회하는 함수만 있어서 따로 구현
+        void printNodeByText(TreeNode parentNode, string nodeText)
+        {
+            foreach (TreeNode node in parentNode.Nodes)
+            {
+                if (node.Text == nodeText)
+                {
+                    foreach (TreeNode nod in node.Nodes)
+                    {
+                        Console.WriteLine(nod.Text);
+                    }
+                    Console.WriteLine("--------------------------");
+                }
+                else
+                {
+                    printNodeByText(node, nodeText);
+                }
+            }
+        }
+
+        // CONTOUR의 node text로 entity를 잘라내 list로 리턴하는 함수
+        List<double> findEntityByIndex(TreeNode parentNode, int index) 
+        {
+            List<double> tnList = new List<double>();
+            bool isWhatImTalkin = false;
+            foreach (TreeNode node in parentNode.Nodes)
+            {
+                if (node.Index == index)
+                {
+                    isWhatImTalkin = true;
+                }
+                if (node.Text.StartsWith("START_U=") || node.Text.StartsWith("U=") || node.Text.StartsWith("START_V=") || node.Text.StartsWith("V="))
+                {
+                    tnList.Add(double.Parse(node.Text.Split('=')[1]));
+                }
+                
+                if (node.Text.StartsWith("V="))
+                {
+                    if (isWhatImTalkin)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        tnList.RemoveAt(0);
+                        tnList.RemoveAt(0);
+                    }
+                }
+            }
+            return tnList;
+        }
+
+        // 트리 전체 조회하는 함수
+        void printAllNodes(TreeNode parentNode)
+        {
+            foreach (TreeNode node in parentNode.Nodes)
+            {
+                Console.WriteLine(node.Text);
+                printAllNodes(node);
+            }
+            if (parentNode.Nodes.Count != 0)
+            {
+                Console.WriteLine("END_OF_{0}", parentNode.Text);
+            }
+        }
+
+        // 모든 CONTOUR와 STRING을 그리는 함수
+        void parseAndDraw(TreeNode parentNode)
+        {
+            foreach (TreeNode node in parentNode.Nodes)
+            {
+                if (node.Text == "CONTOUR")
+                {
+                    List<Dictionary<string, double>> points = new List<Dictionary<string, double>>();
+                    points.Add(new Dictionary<string, double>());
+                    int index = 0; // 다루고있는 딕셔너리의 주소를 가리키는 index
+                    foreach (TreeNode nod in node.Nodes)
+                    {
+                        if (!nod.Text.Contains('=')) // bevel같이 하위노드를 가지는 노드는 =을 안가져서 뺀다.
+                        {
+                            continue;
+                        }
+                        string[] kvPair = nod.Text.Split('='); // key와 value
+                        if (kvPair[0]=="AMP_U")
+                        {
+                            points.Add(new Dictionary<string, double>());
+                            index++;                            
+                        }
+                        points[index].Add(kvPair[0], double.Parse(kvPair[1]));
+                    }
+                    drawLines(points);
+                }
+                else if (node.Text == "STRING")
+                {
+                    Dictionary<string, string> properties = new Dictionary<string, string>();
+                    foreach (TreeNode nod in node.Nodes)
+                    {
+                        string[] kvPair = nod.Text.Split('='); // key와 value
+                        properties.Add(kvPair[0], kvPair[1]);
+                    }
+                    writeString(properties);
+                }
+                else
+                {
+                    parseAndDraw(node);
+                }
+            }
+        }
+
+        // list 받아서 그림그리는 함수
+        void drawLines(List<Dictionary<string, double>> points)
+        {
+            double u = points[0]["START_U"];
+            double v = points[0]["START_V"];
+            int i=1;
+            while(i<points.Count)
+            {
+                if (points[i]["RADIUS"] == 0)
+                {
+                    //ui_canvas2D.DrawCanvas.SelectEntity();
+                    Console.WriteLine(ui_canvas2D.DrawCanvas.AddLine(u, v, points[i]["U"], points[i]["V"]));
+                }
+                else
+                {
+                    if (points[i]["AMP"] < 0)
+                    {
+                        Console.WriteLine(ui_canvas2D.DrawCanvas.AddArc(points[i]["ORIGIN_U"], points[i]["ORIGIN_V"], -points[i]["RADIUS"], u, v, points[i]["U"], points[i]["V"]));
+                    }
+                    else
+                    {
+                        Console.WriteLine(ui_canvas2D.DrawCanvas.AddArc(points[i]["ORIGIN_U"], points[i]["ORIGIN_V"], points[i]["RADIUS"], u, v, points[i]["U"], points[i]["V"]));
+                    }                    
+                }
+                u = points[i]["U"];
+                v = points[i]["V"];
+                i += 1;
+            }
+        }
+
+        // string데이터 삽입
+        void writeString(Dictionary<string, string> properties)
+        {
+            Console.WriteLine(ui_canvas2D.DrawCanvas.AddText(double.Parse(properties["STRING_POSITION_U"]), double.Parse(properties["STRING_POSITION_V"]), double.Parse(properties["STRING_HEIGHT"]), double.Parse(properties["STRING_ANGLE"]), 0, properties["STRING"]));            
+        }
+        
+        // 노드 선택하면 캔버스에 해당 노드 색이 빨간색으로 변하는 함수
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            // 1. TreeNode Contour ->  시작 좌표  U, V
+            // 2. 화면에 있는 부재 형상을 가져와서
+            // 3. U,V 동일한 시작점을 가진 Entity을 찾아서 선택함
+            int hDC = ui_canvas2D.DrawCanvas.GetHdc();
+            if (e.Node.Parent == null || e.Node.Parent.Text != "CONTOUR")
+            {
+                ui_canvas2D.DrawCanvas.DeselectAll(hDC);
+                return;
+            }
+
+            List<double> points = findEntityByIndex(e.Node.Parent, e.Node.Index);
+            List<Entity> entityAllList = ui_canvas2D.DrawCanvas.ViewModel.ToList();
+            Entity selectedEntity = null;
+            Console.WriteLine("x : {0}, y : {1}", points[0], points[1]);
+            foreach (Entity entity in entityAllList)
+            {
+                // Console.WriteLine("entity sx : {0}, sy : {1}", entity.Sx, entity.Sy);
+                // U,V와 동일한 Entity Break
+                if (entity.Sx == points[0] && entity.Sy == points[1])
+                {
+                    selectedEntity = entity;
+                    break;
+                }
+            }
+
+            ui_canvas2D.DrawCanvas.DeselectAll(hDC);
+            ui_canvas2D.DrawCanvas.SelectEntity(hDC, selectedEntity);
+            ui_canvas2D.DrawCanvas.ReleaseHdc(hDC);
+        }
+
+        #region Event
+        #endregion
+    }
+}
